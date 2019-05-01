882a4e61ecbf5eadf1b69289589ea6a59638405b
vrbaj <vrbik.vrbik@gmail.com>
2019-05-01 17:39:56 +0200

    update

--- data_worker.py
+++ data_worker.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 data_set = pd.read_csv('dataset.csv', usecols=['UGDS', 'UG25ABV'])
 data_set.columns = ['pocet', 'starsi25']
 data_set.head()
-print(data_set.describe())
+print('Statistika: \n', data_set.describe())
+print('Korelace: \n', data_set.corr())
+print('Info o datech: ', data_set.keys())
+# check https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
 
 data_set.plot(kind='scatter', x='starsi25', y='pocet', color='Blue')
 plt.tight_layout()
